391a6a9cffd99aa9687c8ed4b92e111a1e90b084
qubaolai <2424419428@qq.com>
2020-05-08 23:38:52 +0800

    提交项目搭建

---
+++ phone-management/src/main/java/com/liuyang/phonemanagement/commons/exception/exceptions/NoLoginException.java
@@ -0,0 +1,27 @@
+package com.liuyang.phonemanagement.commons.exception.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NoLoginException extends RuntimeException{
+    private Integer code;
+    private String msg;
+
+    public NoLoginException(String method) {
+        /**
+         * 存在异常处理类,无需存储状态信息
+         */
+        log.info("用户未登录,接口:{}", method);
+    }
+    public NoLoginException(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+    public Integer getCode(){
+        return code;
+    }
+    @Override
+    public String getMessage() {
+        return msg;
+    }
+}
